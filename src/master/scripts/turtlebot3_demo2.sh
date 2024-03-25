48b5ed1bf989518e5a264ce72109bae9b42c0896
Pablo <velaenator@gmail.com>
2024-03-25 12:29:32 +0800

    a

---
+++ src/master/scripts/turtlebot3_demo2.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/env bash
+
+# We remove a folder that otherwise gives issues in ROS2 launches
+sudo rm -r /home/user/.ros
+
+# Check if the first argument is 'debug'
+if [ "$1" = "debug" ]; then
+    #export ROS2_WS_PATH=/home/user/ros2_ws
+    export ROS2_WS_PATH=/home/user/ros2_ws
+else
+    export ROS2_WS_PATH=/home/simulations/ros2_sims_ws
+fi
+
+# We set up the environment for ROS2
+. /usr/share/gazebo/setup.sh
+. ${ROS2_WS_PATH}/install/setup.bash
+
+export GAZEBO_RESOURCE_PATH=${ROS2_WS_PATH}/src/t3_humble_simulation_pkgs/turtlebot3/turtlebot3_simulations/turtlebot3_gazebo:${ROS2_WS_PATH}/src/t3_humble_simulation_pkgs/turtlebot3/turtlebot3_description:${GAZEBO_RESOURCE_PATH}
+export GAZEBO_MODEL_PATH=${ROS2_WS_PATH}/src/t3_humble_simulation_pkgs/turtlebot3/turtlebot3_simulations/turtlebot3_gazebo/models:${GAZEBO_MODEL_PATH}
+export TURTLEBOT3_MODEL=burger
+ros2 launch turtlebot3_gazebo turtlebot3_world_kombini.launch.py
+
